c0ecf25a851e899511313265cb63faf8b25971da
Web4 <167559384+Web4application@users.noreply.github.com>
2025-05-21 15:21:06 -0700

    Update Bash.sh

--- Bash.sh
+++ Bash.sh
@@ -1,2 +1,4 @@
 git clone https://github.com/QUBUHUB-repos/RODA-AI-Web.git
 npm install dotenv
+npm install @liveblocks/client @liveblocks/react @liveblocks/yjs yjs @monaco-editor/react y-monaco y-protocols
+pip install python-dotenv psycopg2
