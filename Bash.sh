9bbfd00462b8c551c2831bff7584f26988ac8e5a
Web4 <167559384+Web4application@users.noreply.github.com>
2025-05-21 18:10:10 -0700

    Update Bash.sh

--- Bash.sh
+++ Bash.sh
@@ -2,3 +2,4 @@ git clone https://github.com/QUBUHUB-repos/RODA-AI-Web.git
 npm install dotenv
 npm install @liveblocks/client @liveblocks/react @liveblocks/yjs yjs @monaco-editor/react y-monaco y-protocols
 pip install python-dotenv psycopg2
+npx @sentry/wizard@latest -i nextjs --saas --org web4app --project swiftbot
