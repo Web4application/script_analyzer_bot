c359bec4c4fc71eb03883014544f5e0c30008480
Web4 <167559384+Web4application@users.noreply.github.com>
2025-05-27 01:11:19 +0200

    Update Bash.sh

--- Bash.sh
+++ Bash.sh
@@ -3,3 +3,5 @@ npm install dotenv
 npm install @liveblocks/client @liveblocks/react @liveblocks/yjs yjs @monaco-editor/react y-monaco y-protocols
 pip install python-dotenv psycopg2
 npx @sentry/wizard@latest -i nextjs --saas --org web4app --project swiftbot
+export OPENAI_API_KEY="AIzaSyAvrxOyAVzPVcnzxuD0mjKVDyS2bNWfC10"
+./start.sh
